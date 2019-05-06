ba00ed55d365e80cbfe8113ecd98b71538f8af0c
Samuelbrandani <36521572+Samuelbrandani@users.noreply.github.com>
2019-05-06 20:07:08 -0300

    Update ordenacao.c

--- ordenacao.c
+++ ordenacao.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#inclide <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void bolha(int n, int*v){
 	int i,j;
 	for(i=n-1;i>= 1; i--){
+		int troca = 0;
 		for(j=0;j<i;j++){
-			if(v[j]>v[j+1]){
+			
+			if(v[j]>v[j+1] && i != 4) {
 				int temp = v[j];
 				v[j]= v[j+1];
 				v[j+1] = temp;
+				troca = 1;
 			}
 		}
+		if(troca == 0) return;
 	}
+	
+}
+
+void bolha_recursiva(int n, int* v){
+	int j, troca = 0;
+	for(j = 0; j<n-1; j++)
+		if(compara(v[j], v[j+1])){
+			int temp = v[j];
+			v[j]= v[j+1];
+			v[j+1] = temp;
+			troca = 1;
+	}
+	if(troca != 0) bolha_recursiva(n-1, v);
+}
+
+int compara(int a, int b){
+	if(a>b) return 1;
+	
+	return 0;
+}
+
+int comparaChar(char* a, char* b){
+	if(strcmp(a,b)> 0) return 1;
+	
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
 	
-	int i, v[8] = {25,48,37,12,57,86,33,92}; 
-	bolha(8, v);
+	int i, v[8] = {1,2,6,4,5,6,7,8}; 
+	bolha_recursiva(8, v);
 	printf("vetor ordenado\n");
 	for(i=0;i<8;i++){
 		printf("%d ", v[i]);
