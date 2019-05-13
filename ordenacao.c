11a9f146c2a774ddad131c9e3f42055124945d03
Samuel Brandani <36521572+Samuelbrandani@users.noreply.github.com>
2019-05-13 19:18:59 -0300

    Update ordenacao.c

--- ordenacao.c
+++ ordenacao.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #define N 8
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void bolha_gen(int n, void* v, int tam, int (*cmp)(void*,void*));
 int compara_int(void*, void* b);
@@ -15,20 +14,24 @@ void* troca(void*a, void*b, int tam);
 int main(int argc, char *argv[]) {
 	
 	int i, v[8] = {31,212,62,34,45,16,73,8231}; 
-	bolha_gen(N,v, sizeof(int), compara_int);
-	printf("vetor ordenado\n");
-	for(i=0;i<8;i++){
+	printf("\nVetor original: ");
+	for(i=0;i<N;i++){
 		printf("%d ", v[i]);
 	}
+	bolha_gen(N, v, sizeof(int), compara_int);
+	printf("\nVetor ordenado: ");
+	for(i=0;i<N;i++){
+		printf("%d ", v[i]);
+	}
+	printf("\n");
 	system("pause");
-	return 0;
 }
 
 void bolha_gen(int n, void*v, int tam, int (*cmp) (void*, void*)){
 	int i,j;
 	for(i=n-1;i>0;i--){
 		int fez_troca = 0;
-		for(j=0;j<i; i++){
+		for(j=0;j<i; j++){
 			void* p1 = acessa(v, j, tam);
 			void* p2 = acessa(v, j+1, tam);
 			if(cmp(p1,p2)){
@@ -51,13 +54,13 @@ int compara_int(void* a, void* b){
 
 void* acessa(void* v, int i, int tam){
 	char* t = (char*) v;
-	t += tam* i;
+	t += tam*i;
 	return (void*) t;
 }
 
 void* troca(void* a, void* b, int tam){
-	char* v1 = (char*)a;
-	char* v2 = (char*)b;
+	char* v1 = (char*) a;
+	char* v2 = (char*) b;
 	int i;
 	for(i=0;i<tam;i++){
 		char temp = v1[i];
